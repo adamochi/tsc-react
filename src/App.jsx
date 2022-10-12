8a08fb36bda58c76188a65a0072cf3604a36ebc4
adamochi <honeyrumballs@gmail.com>
2022-10-12 17:43:39 +1000

    Learning typescript

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Potato from "./potato/Potato.tsx";
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <Potato />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/potato/Potato.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./potato.scss";
+
+type TODO = {
+  name: string;
+  readonly id: number;
+};
+const Potato = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([] as TODO[]);
+  const banana = useRef(document.createElement("input"));
+
+  useEffect(() => {
+    const saved = localStorage.getItem("todos");
+    console.dir(saved);
+    if (saved !== null) {
+      const parsed = JSON.parse(saved);
+      return setTodos(parsed);
+    } else {
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    return localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleSubmit = (event: any): any => {
+    event.preventDefault();
+    const onlySpaces = (str: string) => {
+      return str.trim().length === 0;
+    };
+    const data: string = banana.current.value;
+    if (data === "" || onlySpaces(data)) return;
+    const newTodoObj: TODO = {
+      name: todo,
+      id: Date.now(),
+    };
+    setTodos([newTodoObj, ...todos]);
+    setTodo("");
+    return (banana.current.value = "");
+  };
+
+  const handleOnChange = (e: { target: { value: string } }) => {
+    setTodo(e.target.value.trim());
+  };
+
+  const handleRemove = (e: { target: { id: string } }) => {
+    const id = JSON.parse(e.target.id);
+    setTodos(todos.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={banana}
+          id="input"
+          type="text"
+          placeholder="type something"
+          onChange={handleOnChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div className="todos_outer-div">
+        {todos.map((item) => (
+          <div id="todos_div" key={item.id}>
+            <div>
+              <span className="item_span">{item.name}</span>
+              <span className="time-of_span">
+                {" "}
+                @ {new Date(item.id).getHours().toString().padStart(2, "0")}:
+                {new Date(item.id).getMinutes().toString().padStart(2, "0")}
+                {new Date(item.id).getHours() > 11 ? "pm" : "am"}
+              </span>
+            </div>
+            <button id={`${item.id}`} onClick={handleRemove}>
+              remove
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Potato;
