7628c259a2af87322529dbebb2a641290787494b
adamochi <honeyrumballs@gmail.com>
2022-10-15 14:03:05 +1000

    html title

--- src/components/learning/classes.tsx
+++ src/components/learning/classes.tsx
@@ -1,4 +1,32 @@
-abstract class User {
+type Words = {
+  [key: string]: string;
+};
+
+class Dictionary {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term: string) {
+    return this.words[term];
+  }
+}
+
+class Word {
+  constructor(public term: string, public def: string) {}
+}
+
+const kimchi = new Word("kimchi", "food from korea");
+const dictionary = new Dictionary();
+dictionary.add(kimchi);
+console.log(dictionary.def("kimchi"));
+
+/* abstract class User {
   constructor(
     protected firstName: string,
     private lastName: string,
@@ -19,3 +47,4 @@ class Player extends User {
 const adam = new Player("Adam", "Sullivan", "adamochi");
 console.log(adam.nickname);
 console.log(adam.getFullName()); // could make this function inside of the abstract class ↑↑ private as well so this console.log() wouldn't work
+ */
